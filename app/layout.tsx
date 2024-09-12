54c42e29a55c96d9d75a2eec2ce899707870cc04
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-09-12 22:48:14 +0500

    add some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const session = await auth()
+  const session = await auth();
 
   return (
     <SessionProvider session={session}>
